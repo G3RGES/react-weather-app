abe675c0df1f41e50055535e76607514f42e9f70
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-03 23:43:37 +0200

    Update WeatherApp.jsx

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherApp.css";
 
 // assets
@@ -13,6 +13,9 @@ import humidity_icon from "../Assets/humidity.png";
 
 const WeatherApp = () => {
   let api_key = "17ee74de4fcf210b11c11537c8dd64f9";
+
+  const [wicon, setWicon] = useState(cloud_icon);
+
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
     if (element[0].value === "") {
@@ -27,13 +30,53 @@ const WeatherApp = () => {
     const temp = document.getElementsByClassName("weather-temp");
     const location = document.getElementsByClassName("weather-location");
 
-    humidity[0].innerHTML = data.main.humidity;
-    wind[0].innerHTML = data.wind.speed;
-    temp[0].innerHTML = data.main.temp;
+    //* THIS WAY WAS USED SO THER'S ALWAYS A VALUE SHOWN (DEFAULT VALUE,PROBABLY WRONG TOO)
+    //* UNTIL A CITY IS SEARCHED FOR AND VALUES (CURRENT VALUES) ARE SHOWN
+    humidity[0].innerHTML = data.main.humidity + " %";
+    wind[0].innerHTML = Math.floor(data.wind.speed) + " Km/h";
+    temp[0].innerHTML = Math.floor(data.main.temp) + "°c";
     location[0].innerHTML = data.name;
+
+    //* SETTING THE ICONS USING USESTATE AND CHECKING THE WEATHER ICON ID FROM THE JSON
+    //* FILE IN THE API
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+      // console.log(data.weather[0].icon);//TESTING
+      setWicon(clear_icon);
+    } else if (
+      data.weather[0].icon === "02d" ||
+      data.weather[0].icon === "02n"
+    ) {
+      setWicon(cloud_icon);
+    } else if (
+      data.weather[0].icon === "03d" ||
+      data.weather[0].icon === "03n"
+    ) {
+      setWicon(drizzle_icon);
+    } else if (
+      data.weather[0].icon === "04d" ||
+      data.weather[0].icon === "04n"
+    ) {
+      setWicon(drizzle_icon);
+    } else if (
+      data.weather[0].icon === "09d" ||
+      data.weather[0].icon === "09n"
+    ) {
+      setWicon(rain_icon);
+    } else if (
+      data.weather[0].icon === "10d" ||
+      data.weather[0].icon === "10n"
+    ) {
+      setWicon(snow_icon);
+    } else if (
+      data.weather[0].icon === "13d" ||
+      data.weather[0].icon === "13n"
+    ) {
+      setWicon(snow_icon);
+    } else {
+      setWicon(clear_icon);
+    }
   };
-  //* THIS WAY WAS USED SO THER'S ALWAYS A VALUE SHOWN (DEFAULT VALUE,PROBABLY WRONG TOO)
-  //* UNTIL A CITY IS SEARCHED FOR AND VALUES (CURRENT VALUES) ARE SHOWN
+
   return (
     <div className="container ">
       <div className="top-bar">
@@ -43,7 +86,7 @@ const WeatherApp = () => {
         </div>
       </div>
       <div className="weather-image">
-        <img src={cloud_icon} alt="" />
+        <img src={wicon} alt="" />
       </div>
       <div className="weather-temp">24°c</div>
       <div className="weather-location">London</div>
